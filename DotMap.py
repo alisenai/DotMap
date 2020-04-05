1d007f78b2f588f2050ff9083f4824d4bbc58d08
Alien G <calingavriliuc@gmail.com>
2020-04-05 01:10:52 -0700

    Added some comments.

--- DotMap.py
+++ DotMap.py
@@ -3,12 +3,13 @@ import json
 import shutil
 import argparse
 
+# Better help formatting
 formatter = lambda prog: argparse.HelpFormatter(prog, max_help_position=52)
+# Create a input parser and argument group
 parser = argparse.ArgumentParser(formatter_class=formatter, description="Dot File Manager", add_help=False)
 main_commands = parser.add_mutually_exclusive_group()
-main_commands.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS,
-                           help='Show help')
-# action='store_true',
+# Add arguments
+main_commands.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS, help='Show help')
 main_commands.add_argument('--install', metavar="{GROUP | ALL}", help="Installs dot files")
 main_commands.add_argument('--create-group', metavar="{NAME}", help="Creates dot file group")
 main_commands.add_argument('--delete-group', metavar="{GROUP}", help="Deletes dot file group")
@@ -17,9 +18,11 @@ main_commands.add_argument('--delete-dot', metavar=("{GROUP}", "{SAVE_DIR}"), na
 parser.add_argument('--list-groups', action='store_true', help="Lists dot file groups")
 parser.add_argument('--list-dots', metavar="{GROUP}", help="Lists dot files within a given group")
 parser.add_argument('--list', action='store_true', help="Lists all stored dot files")
+# Parse inputs
 args = parser.parse_args()
 
 
+# Installs a given group's dots
 def install_group(group: str, dots_data):
     if group in dots_data:
         print("  [Installing group: %s]" % group.lower())
@@ -32,7 +35,6 @@ def install_group(group: str, dots_data):
                print("      [%s exists! Creating backup]" % dest_file)
                shutil.move(dest_file, dest_file.rstrip('\\').rstrip('/')+".dotback")
                print("      [Done]")
-               # print(os.path.dirname(os.path.abspath(dest_file)))
             os.system("ln -s '%s' '%s'" % (source_file, os.path.abspath(dest_file)))
             print("    [Done]")
         print("  [Done]")
@@ -40,6 +42,7 @@ def install_group(group: str, dots_data):
         print("  [Group %s not found]" % group)
 
 
+# Adds a new dot to manage
 def add_dot(group: str, source_dir: str, save_dir: str, dots_data):
     # source_dir = os.path.abspath(source_dir)
     if group in dots_data:
@@ -55,7 +58,6 @@ def add_dot(group: str, source_dir: str, save_dir: str, dots_data):
                 print("[Source file not found]")
         elif os.path.isdir(source_dir):
             try:
-                #  os.makedirs(os.path.dirname(save_dir), exist_ok=True)
                 shutil.copytree(source_dir, save_dir)
                 dots_data[group][source_dir] = save_dir
                 print("[Added dot]")
@@ -70,6 +72,7 @@ def add_dot(group: str, source_dir: str, save_dir: str, dots_data):
         print("  [Group %s not found]" % group)
 
 
+# Lists all tracked dots
 def list_dots(group: str, indent_count: int, dots_data):
     if group in dots_data:
         for i in range(indent_count):
@@ -89,14 +92,16 @@ def list_dots(group: str, indent_count: int, dots_data):
         print("[Group %s not found]" % group)
 
 
-
+# Read data json
 with open("dots/dots.json") as dots_file:
     dots_data = json.load(dots_file)
 
+# Addding a dot to a group
 if args.add_dot:
     add_dot(args.add_dot[0], args.add_dot[1], args.add_dot[2], dots_data)
 
 
+# Deleting a dot from a group
 if args.delete_dot:
     group, dot = args.delete_dot[0], args.delete_dot[1]
     if group in dots_data:
@@ -108,16 +113,20 @@ if args.delete_dot:
     else:
         print("[Group %s not found]" % group)
 
+# Creates a dot group
 if args.create_group:
     print("[Creating group: %s]" % args.create_group, end='')
     dots_data[args.create_group] = {}
     print("[Ok]")
 
+# Deltes a dot group
 if args.delete_group:
     print("[Deleting group: %s]" % args.delete_group, end='')
     del dots_data[args.delete_group]
+    # TODO: Delete associated data from dots folder
     print("[Ok]")
 
+# Lists managed groups and dots
 if args.list:
     print("[DotMap:]")
     if len(dots_data) > 0:
@@ -126,14 +135,17 @@ if args.list:
     else:
         print("  [No dots stored]")
 
+# List managed groups
 if args.list_groups:
     print("[Groups:]")
     for group in dots_data:
         print("- %s" % group)
 
+# List managed dots
 if args.list_dots:
     list_dots(args.list_dots, 0, dots_data)
 
+# Install dots
 if args.install:
     print("[Installing dot files]")
     if args.install.lower() == "all":
